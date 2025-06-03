ee393adcfa9b858a21d4903948f8c7fbff3f1389
NOOB321470 <116083722+NOOB321470@users.noreply.github.com>
2025-06-03 17:58:28 +0300

    Add files via upload

---
+++ zadanie 03.06.25.py
@@ -0,0 +1,21 @@
+def make_negative(num):
+    if num > 0:
+        return -num
+    else:
+        return num
+print(make_negative(3))
+print(make_negative(0))
+print(make_negative(-2))
+
+
+def bet(fir_num, sec_num):
+    numb = 1
+    while fir_num != sec_num:
+        numb += 1
+        spisok = [bet(numb)]
+    return [spisok]
+
+spisok = []
+arg = 
+spisok.append(1)
+print(spisok)
